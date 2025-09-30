956fc7842d85ce4f2dca0dcce291c0af39574bcf
Sebbbenator <eaa25ssg@students.eaaa.dk>
2025-09-30 11:22:38 +0200

    fejl rettet

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const barbieMovie = {
   genre: ["Adventure", "Comedy", "Fantasy"],
   rating: 7.0,
   director: "Greta Gerwig",
-  image: "https://upload.wikimedia.org/wikipedia/en/0/0b/Barbie_2023_poster.jpg",
+  image:
+    "https://upload.wikimedia.org/wikipedia/en/0/0b/Barbie_2023_poster.jpg",
   actors: ["Margot Robbie", "Ryan Gosling", "America Ferrera"],
   description:
-    "Barbie and Ken embark on a journey of self-discovery after leaving the utopian Barbie Land for the real world."
+    "Barbie and Ken embark on a journey of self-discovery after leaving the utopian Barbie Land for the real world.",
 };
 
 console.log("Barbie movie object:", barbieMovie);
@@ -25,10 +26,11 @@ const duneMovie = {
   genre: ["Adventure", "Drama", "Sci-Fi"],
   rating: 8.0,
   director: "Denis Villeneuve",
-  image: "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
+  image:
+    "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
   actors: ["Timothée Chalamet", "Rebecca Ferguson", "Oscar Isaac"],
   description:
-    "Paul Atreides leads nomadic tribes in a battle to control the desert planet Arrakis and its valuable spice."
+    "Paul Atreides leads nomadic tribes in a battle to control the desert planet Arrakis and its valuable spice.",
 };
 
 console.log("Dune movie object:", duneMovie);
@@ -45,7 +47,7 @@ const duneTwoMovie = {
     "https://m.media-amazon.com/images/M/MV5BNTc0YmQxMjEtODI5MC00NjFiLTlkMWUtOGQ5NjFmYWUyZGJhXkEyXkFqcGc@._V1_.jpg",
   actors: ["Timothée Chalamet", "Zendaya", "Rebecca Ferguson"],
   description:
-    "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family."
+    "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.",
 };
 
 console.log("Dune: Part Two movie object:", duneTwoMovie);
@@ -55,15 +57,20 @@ const everythingEverywhereAllatOnce = {
   id: 4, // Unikt nummer for filmen
   title: "Everything Everywhere All at Once",
   year: 2022, // Number, ikke string!
-  genre: ["Action", "Adventure", "Comedy"], // Array med genres, f.eks. ["Action", "Drama"]
-  rating: 7.8, // Number (kan have decimaler)
-  director: "Daniel Kwan", "Daniel Scheinert",
-  image: "https://m.media-amazon.com/images/M/MV5BOWNmMzAzZmQtNDQ1NC00Nzk5LTkyMmUtNGI2N2NkOWM4MzEyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", // URL til poster billede
-  actors: [...], // Array med skuespillere
-  description: "..." // Kort beskrivelse af filmen
+  genre: ["Action", "Adventure", "Comedy"],
+  rating: 7.8,
+  director: ["Daniel Kwan", "Daniel Scheinert"],
+  image:
+    "https://m.media-amazon.com/images/M/MV5BOWNmMzAzZmQtNDQ1NC00Nzk5LTkyMmUtNGI2N2NkOWM4MzEyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+  actors: ["Michelle Yeoh", "Stephanie Hsu", "Ke Huy Quan"],
+  description:
+    "An aging Chinese immigrant is swept up in an insane adventure, where she alone can save the world by exploring other universes.",
 };
 
-console.log("Everything Everywhere All at Once:", everythingEverywhereAllatOnce);
+console.log(
+  "Everything Everywhere All at Once:",
+  everythingEverywhereAllatOnce
+);
 
 // Movie 5: Fight Club
 const fightClub = {
@@ -75,7 +82,8 @@ const fightClub = {
   director: "David Fincher",
   image: "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_.jpg",
   actors: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
-  description: "An insomniac office worker and a soap maker form an underground fight club that evolves into something much more."
+  description:
+    "An insomniac office worker and a soap maker form an underground fight club that evolves into something much more.",
 };
 
 console.log("Fight Club:", fightClub);
@@ -84,13 +92,15 @@ console.log("Fight Club:", fightClub);
 const forrestGump = {
   id: 6,
   title: "Forrest Gump",
-  year: 1994
-  genre: ["Drama", "Romance"],
+  year: 1994,
   rating: 8.8,
   director: "Robert Zemeckis",
-  image: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+  image:
+    "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
   actors: ["Tom Hanks", "Robin Wright", "Gary Sinise"],
-  description: "The presidencies of Kennedy and Johnson, the Vietnam War, and other historical events unfold from the perspective"
+  genre: ["Drama", "Romance"],
+  description:
+    "The presidencies of Kennedy and Johnson, the Vietnam War, and other historical events unfold from the perspective of an Alabama man with an IQ of 75.",
 };
 
 console.log("Forrest Gump:", forrestGump);
@@ -105,7 +115,8 @@ const goodFellas = {
   director: "Martin Scorsese",
   image: "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg",
   actors: ["Robert De Niro", "Ray Liotta", "Joe Pesci"],
-  description: "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate."
+  description:
+    "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
 };
 
 console.log("Goodfellas:", goodFellas);
@@ -115,12 +126,14 @@ const inception = {
   id: 8,
   title: "Inception",
   year: 2010,
-  genre: ["Action", "Adcenture", "Sci-Fi"],
   rating: 8.8,
   director: "Christopher Nolan",
-  image: "https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg",
+  image:
+    "https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg",
   actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
-  description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O."
+  description:
+    "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+  genre: ["Action", "Adventure", "Sci-Fi"],
 };
 
 console.log("Inception:", inception);
